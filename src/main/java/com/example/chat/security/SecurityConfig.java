f806f78c0e8c9a46bc03d1ebdc685943d99a4516
Maxim Kolesov <pro100maks222@gmail.com>
2023-03-22 11:55:36 +0300

    Ignore stomp auth

--- src/main/java/com/example/chat/security/SecurityConfig.java
+++ src/main/java/com/example/chat/security/SecurityConfig.java
@@ -28,6 +28,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(
                         authorize -> authorize
                                 .requestMatchers(url + "/auth/login", url + "/auth/register", url + "/auth/refresh").permitAll()
+                                .requestMatchers("/stomp/**").permitAll()
                                 .anyRequest().authenticated()
                 )
                 .cors().and()
