5b202ab6b23926042aba8bec853fc145fe5a4fa2
Maxim Kolesov <pro100maks222@gmail.com>
2023-03-22 12:49:29 +0300

    Add userService

--- src/main/java/com/example/chat/repository/UserRepository.java
+++ src/main/java/com/example/chat/repository/UserRepository.java
@@ -4,8 +4,11 @@ import com.example.chat.model.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, UUID> {
+    boolean existsUserByUsername(String username);
+    Optional<User> findUserByUsername(String username);
 }
---
+++ src/main/java/com/example/chat/service/UserService.java
@@ -0,0 +1,65 @@
+package com.example.chat.service;
+
+import com.example.chat.exception.UserAlreadyExistsException;
+import com.example.chat.model.User;
+import com.example.chat.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsManager {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+
+    @Override
+    public void createUser(UserDetails user) throws UserAlreadyExistsException {
+        if (userExists(user.getUsername())) {
+            throw new UserAlreadyExistsException(String.format("%s already exists", user.getUsername()));
+        }
+
+        ((User)user).setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save((User)user);
+    }
+
+    @Override
+    public void updateUser(UserDetails user) throws UsernameNotFoundException {
+        if(!userExists(user.getUsername())) {
+            throw new UsernameNotFoundException(String.format("%s not found", user.getUsername()));
+        }
+
+        userRepository.save((User)user);
+    }
+
+    @Override
+    public void deleteUser(String username) {
+        if (!userExists(username)) {
+            throw new UsernameNotFoundException(String.format("User %s not found", username));
+        }
+
+        User user = (User) loadUserByUsername(username);
+        userRepository.delete(user);
+    }
+
+    @Override
+    public void changePassword(String oldPassword, String newPassword) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean userExists(String username) {
+        return userRepository.existsUserByUsername(username);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findUserByUsername(username).orElseThrow(() -> new UsernameNotFoundException(String.format("%s not found", username)));
+    }
+
+}
